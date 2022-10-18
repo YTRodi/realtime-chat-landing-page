b0a18a8a5f30ceb6f31996b3c220987c2611b734
YTRodi <yagorodi@gmail.com>
2022-10-18 14:45:02 -0300

    feat: navbar and hero animation

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,48 +1,35 @@
-import gsap from 'gsap';
-import { useEffect } from 'react';
-
 import { BRAND_NAME, NAVBAR_HEIGHT } from '../../constants';
 
-const Navbar = () => {
-	useEffect(() => {
-		const ctx = gsap.context(() => {
-			gsap.from('.header', { y: NAVBAR_HEIGHT, duration: 1, ease: 'back' });
-		});
-
-		return () => ctx.revert();
-	}, []);
-
-	return (
-		<header className='header' style={{ height: `${NAVBAR_HEIGHT}px` }}>
-			<nav className='nav'>
-				<div className='nav__logo'>
-					<p className='nav__img'>S</p>
-					<p className='nav__brand-name'>{BRAND_NAME}</p>
-				</div>
-				<ul className='nav__links'>
-					<li className='nav__link'>
-						<a href=''>How it works</a>
-					</li>
-					<li className='nav__link'>
-						<a href=''>Library</a>
-					</li>
-					<li className='nav__link'>
-						<a href=''>Pricing</a>
-					</li>
-				</ul>
-			</nav>
-			<div>
-				<ul className='auth__links'>
-					<li className='auth__link'>
-						<a href=''>Sign up</a>
-					</li>
-					<li className='auth__link'>
-						<a href=''>Login</a>
-					</li>
-				</ul>
+const Navbar = () => (
+	<header className='header' style={{ height: `${NAVBAR_HEIGHT}px` }}>
+		<nav className='nav'>
+			<div className='nav__logo'>
+				<p className='nav__img'>S</p>
+				<p className='nav__brand-name'>{BRAND_NAME}</p>
 			</div>
-		</header>
-	);
-};
+			<ul className='nav__links'>
+				<li className='nav__link'>
+					<a href=''>How it works</a>
+				</li>
+				<li className='nav__link'>
+					<a href=''>Library</a>
+				</li>
+				<li className='nav__link'>
+					<a href=''>Pricing</a>
+				</li>
+			</ul>
+		</nav>
+		<div>
+			<ul className='auth__links'>
+				<li className='auth__link'>
+					<a href=''>Sign up</a>
+				</li>
+				<li className='auth__link'>
+					<a href=''>Login</a>
+				</li>
+			</ul>
+		</div>
+	</header>
+);
 
 export default Navbar;
