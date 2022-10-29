73c7ed2b7fcebbf2368b772ec59a42a67153718e
YTRodi <yagorodi@gmail.com>
2022-10-29 15:06:16 -0300

    feat: update Pills and ChatBubble components

--- src/components/ChatBubble/index.tsx
+++ src/components/ChatBubble/index.tsx
@@ -1,10 +1,11 @@
 interface Props {
 	text: string;
-	position?: 'left' | 'right';
+	color?: 'primary' | 'secondary';
+	kind?: 'sender' | 'receiver';
 }
 
-const ChatBubble = ({ text, position = 'left' }: Props) => (
-	<div className={`chat-bubble chat-bubble--${position}`}>
+const ChatBubble = ({ text, color = 'primary', kind = 'sender' }: Props) => (
+	<div className={`chat-bubble chat-bubble--${kind} chat-bubble--${color}`}>
 		<h5>{text}</h5>
 	</div>
 );
--- src/components/Pills/index.tsx
+++ src/components/Pills/index.tsx
@@ -4,15 +4,23 @@ const Pills = () => (
 	<div className='grid-pills'>
 		<div className='grid-pills__cell' />
 		<div className='grid-pills__cell'>
-			<ChatBubble text='Hi Karen, I’m wandering is it possible to order custom drawing?' />
+			<div className='grid-pills__chat-bubbles grid-pills__chat-bubbles--left'>
+				<ChatBubble text='Hi Karen, I’m wandering is it possible to order custom drawing?' />
+			</div>
 			<img
 				alt='girl chatting on a computer'
 				src='/assets/main-content/main-content-2.png'
 			/>
 		</div>
 		<div className='grid-pills__cell'>
-			<ChatBubble position='right' text='Hi Samantha' />
-			<ChatBubble position='right' text='Yes, You can order custom drawing' />
+			<div className='grid-pills__chat-bubbles grid-pills__chat-bubbles--right'>
+				<ChatBubble color='secondary' kind='receiver' text='Hi Samantha' />
+				<ChatBubble
+					color='secondary'
+					kind='receiver'
+					text='Yes, You can order custom drawing'
+				/>
+			</div>
 			<img
 				alt='girl chatting on a computer'
 				src='/assets/main-content/main-content-1.png'
